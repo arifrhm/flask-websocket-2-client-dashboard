701542a0961b7569dcc9d03beab36759b527ede7
Arif Rahman <arif.rahman@delameta.id>
2024-01-15 17:35:13 +0700

    Feat : Decoratorable authentication

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+import os
+from datetime import datetime, timedelta
+
 from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO, emit
 from flask_cors import CORS
-from datetime import datetime, timedelta
 import jwt
-import os
 
 app = Flask(__name__, template_folder=os.curdir)
 cors = CORS(app)
@@ -40,20 +41,6 @@ def smartband():
 def dashboard():
     return render_template('dashboard.html')
 
-# JWT configuration
-def authenticate(username, password):
-    user = users.get(username, None)
-    if user and user['password'] == password:
-        return user
-
-def generate_token(user_id):
-    payload = {
-        'exp': datetime.utcnow() + timedelta(days=1),
-        'iat': datetime.utcnow(),
-        'sub': user_id
-    }
-    return jwt.encode(payload, app.config['SECRET_KEY'], algorithm='HS256')
-
 @app.route('/authenticate', methods=['POST'])
 def handle_authentication():
     data = request.get_json()
@@ -69,28 +56,53 @@ def handle_authentication():
     else:
         return jsonify({'error': 'Invalid credentials'}), 401
 
+# JWT configuration
+def authenticate(username, password):
+    user = users.get(username, None)
+    if user and user['password'] == password:
+        return user
+
+def generate_token(user_id):
+    payload = {
+        'exp': datetime.utcnow() + timedelta(days=1),
+        'iat': datetime.utcnow(),
+        'sub': user_id
+    }
+    return jwt.encode(payload, app.config['SECRET_KEY'], algorithm='HS256')
+
+### WRAPPER BLOCK ###
+
+from functools import wraps
+
+def authenticate_token(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        token = request.args.get('token')
+        try:
+            decoded = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+            user_id = decoded['sub']
+            print(f'{users[user_id]["username"]} connected to /dashboard')
+            return func(*args, **kwargs)
+        except jwt.ExpiredSignatureError:
+            return False
+        except jwt.InvalidTokenError:
+            return False
+    return wrapper
+
+### WRAPPER BLOCK ###
+
+
 @socketio.on('connect', namespace='/dashboard')
+@authenticate_token
 def handle_connect():
-    token = request.args.get('token')
-    try:
-        decoded = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
-        user_id = decoded['sub']
-        print(f'{users[user_id]["username"]} connected to /dashboard')
-    except jwt.ExpiredSignatureError:
-        return False
-    except jwt.InvalidTokenError:
-        return False
+    print(f"{request.sid} Connected !!!!!!!")
 
 @socketio.on('smartband_data', namespace='/dashboard')
+@authenticate_token
 def handle_smartband_data(data):
-    token = request.args.get('token')
-    try:
-        decoded = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
-        user_id = decoded['sub']
-        print(f'Received data from {users[user_id]["username"]}: {data}')
-        emit('update_dashboard', data, broadcast=True, namespace='/dashboard')
-    except:
-        pass
+    print("Emitting to update_dashboard listeners with namespace /dashboard")
+    emit('update_dashboard', data, broadcast=True, namespace='/dashboard')
+    print("Emitted !!!!")
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
